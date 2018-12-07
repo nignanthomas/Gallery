1fb22056569714bb71613e3fe7fcbabb70d4eabb
nignanthomas <nignanthomas@gmail.com>
2018-12-07 11:04:28 +0300

    connect gallery urls to phtos urls; create basic landing view function

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 
 # Create your views here.
+
+
+def landing(request):
+    return render(request, 'index.html')
