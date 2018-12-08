b998d4937d03bbbf1e99415fc9b12a7f3ad0a5c6
nignanthomas <nignanthomas@gmail.com>
2018-12-08 15:57:44 +0300

    update gallery/urls.py to register the MEDIA_ROOT route

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,3 +6,5 @@ urlpatterns = [
     url('^$',views.landing,name='landing'),
     url(r'^search/',views.search_results, name='search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
