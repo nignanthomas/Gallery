f94311f1250c8ed83ccba6c3042fe842a5eba05c
nignanthomas <nignanthomas@gmail.com>
2018-12-09 02:06:00 +0300

    create view functions and url routes for category and location pages

--- gallery/views.py
+++ gallery/views.py
@@ -16,14 +16,32 @@ def landing(request):
 
 
 def search_results(request):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = 'Search'
 
     if 'searchcat' in request.GET and request.GET["searchcat"]:
         search_term = request.GET.get("searchcat")
         searched_images = Image.search_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'index.html',{"message":message,"all_images": searched_images})
+        return render(request, 'index.html',{"message":message,"all_images": searched_images,'locations':locations,'categories':categories, 'title':title})
 
     else:
         message = "You haven't searched for any term."
-        return render(request, 'index.html',{"message":message})
+        return render(request, 'index.html',{"message":message, 'locations':locations,'categories':categories, 'title':title})
+
+
+def page_category(request,category):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = f"{category}"
+    category_results = Image.search_category(category)
+    return render(request,'index.html',{'all_images':category_results,'locations':locations,'categories':categories, 'title':title})
+
+def page_location(request,location):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = f"{location}"
+    location_results = Image.filter_location(location)
+    return render(request,'index.html',{'all_images':location_results,'locations':locations,'categories':categories, 'title':title})
